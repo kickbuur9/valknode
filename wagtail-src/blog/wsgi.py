e342855fda0d719a82e6fd035b751b4e45dbbbf5
kickbuur9 <kickbuur@gmail.com>
2025-10-08 22:24:19 +0200

    final changes

--- wagtail-src/blog/wsgi.py
+++
@@ -1,9 +0,0 @@
-# blog/wsgi.py
-import os
-from django.core.wsgi import get_wsgi_application
-
-# This line tells Django which settings file to use
-# For production, this should be set by the environment
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blog.settings.development") # Default for local dev
-
-application = get_wsgi_application()
