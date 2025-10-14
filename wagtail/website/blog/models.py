6e3e488664b041d1802ef626905a753d0ef752ad
kickbuur9 <kickbuur@gmail.com>
2025-10-14 21:28:54 +0200

    clean start

---
+++ wagtail/website/blog/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+
+
+class BlogIndexPage(Page):
+    intro = RichTextField(blank=True)
+    # add the get_context method:
+    def get_context(self, request):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super().get_context(request)
+        blogpages = self.get_children().live().order_by('-first_published_at')
+        context['blogpages'] = blogpages
+        return context
+
+class BlogPage(Page):
+    date = models.DateField("Post date")
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + ["date", "intro", "body"]
---
+++ wagtail/website/pricing/models.py
@@ -0,0 +1,12 @@
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
+
+class Pricing(Page):
+    intro = RichTextField(blank=True)
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("intro"),
+        FieldPanel("body"),
+    ]
